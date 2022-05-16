aa31bbb58424be76bb7be2d023c0524ae99d7846
Txus77 <66962600+Txus77@users.noreply.github.com>
2022-05-16 18:43:26 +0200

    Add files via upload

---
+++ tarea_94/tarea_94.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+def jupiter (seg):
+    altura=1/2*24.8*seg**2
+    return altura
+print(jupiter(9))
+
+
+# In[7]:
+
+
+def tierra (seg):
+    altura=1/2*9.8*seg**2
+    return altura
+print(tierra(9))
+
+
+# In[6]:
+
+
+def marte (seg):
+    altura=1/2*2.7*seg**2
+    return altura
+print(marte(9))
+
+
+# In[ ]:
+
+
+
+
